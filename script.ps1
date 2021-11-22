47528d4d7e1b2c55a91173b2347a5af71f684190
Ch AFleks <adynetro@gmail.com>
2021-11-22 10:58:56 +0200

    fully worked but still errors

--- script.ps1
+++ script.ps1
@@ -5,7 +5,6 @@ if ([BOOL](Get-WmiObject -Class BatteryStatus -Namespace root\wmi).PowerOnLine )
 else{
       #Device is NOT plugged in now, do this action
       write-host "Now on battery, Running scripts..."
-      Get-PnpDevice -FriendlyName "*Nvidia*" | Disable-PnpDevice 
-      Get-PnpDevice -FriendlyName "*Nvidia*" | Enable-PnpDevice 
-
+      Get-PnpDevice -FriendlyName "NVIDIA Quadro*" | Disable-PnpDevice -confirm:$false # Disable nvidia discrete graphics
+      Get-PnpDevice -FriendlyName "NVIDIA Quadro*" | Enable-PnpDevice -confirm:$false # Re-enable nvidia adapter
    }
