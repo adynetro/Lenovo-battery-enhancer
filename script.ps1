699b3361a00ccaadf91c6c93b02952697c534e0b
Ch AFleks <adynetro@gmail.com>
2021-11-22 10:52:42 +0200

    fixed and tested

--- script.ps1
+++ script.ps1
@@ -1,15 +1,11 @@
--sourceIdentifier "Power" `
--action {
-    if ([BOOL](Get-WmiObject -Class BatteryStatus -Namespace root\wmi).PowerOnLine ){
+if ([BOOL](Get-WmiObject -Class BatteryStatus -Namespace root\wmi).PowerOnLine ){
        #Device is plugged in now, do this action
        write-host "Power on!"
    }
-  else{
+else{
       #Device is NOT plugged in now, do this action
       write-host "Now on battery, Running scripts..."
-      Get-PnpDevice -FriendlyName "*Nvidia*" | Disable-PnpDevice # Enables all PNP Devices with a name containing "Ethernet"
-      Get-PnpDevice -FriendlyName "*Nvidia*" | Enable-PnpDevice # Enables all PNP Devices with a name containing "Ethernet"
+      Get-PnpDevice -FriendlyName "*Nvidia*" | Disable-PnpDevice 
+      Get-PnpDevice -FriendlyName "*Nvidia*" | Enable-PnpDevice 
 
-    #   [NativeMethods]::LockWorkStation()
    }
-}
