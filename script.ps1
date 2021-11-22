58181683b53bec9dfa6aa5c2dcc8f5218fa92897
Ch AFleks <adynetro@gmail.com>
2021-11-22 21:07:58 +0200

    changed elevation method

--- script.ps1
+++ script.ps1
@@ -1,10 +1,7 @@
-If (-NOT ([Security.Principal.WindowsPrincipal][Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole([Security.Principal.WindowsBuiltInRole] "Administrator"))
-{   
-  #"No Administrative rights, it will display a popup window asking user for Admin rights"
-
-  $arguments = "& '" + $myinvocation.mycommand.definition + "'"
-  Start-Process "$psHome\powershell.exe" -Verb runAs -ArgumentList $arguments
-  Break
+if (!([Security.Principal.WindowsPrincipal][Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole([Security.Principal.WindowsBuiltInRole] "Administrator")) 
+{ 
+  Start-Process powershell.exe "-NoProfile -ExecutionPolicy Bypass -File `"$PSCommandPath`"" -Verb RunAs; 
+  break
 }
 
 if ([BOOL](Get-WmiObject -Class BatteryStatus -Namespace root\wmi).PowerOnLine ){
