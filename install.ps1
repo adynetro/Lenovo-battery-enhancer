72a199101d2b8327e6bfcb9ac2bc2abefc085b0d
Alexandru Chiscari <alexandru.chiscari@btrl.ro>
2021-11-23 14:03:31 +0200

    fixed progress bar

--- install.ps1
+++ install.ps1
@@ -4,7 +4,7 @@ if (!([Security.Principal.WindowsPrincipal][Security.Principal.WindowsIdentity]:
   break
 }
 New-Item -Path "$env:appdata\\adynetro\\" -ItemType Directory -Force
-for ($i = 1; $i -le 100; $i++ )
+for ($i = 1; $i -le 50; $i++ )
 
 {
     Write-Progress -Activity "Install in progress..." -Status "$i% Complete:" -PercentComplete $i
@@ -12,9 +12,16 @@ for ($i = 1; $i -le 100; $i++ )
 }
 
 Copy-Item -Path "$PSScriptRoot\*" -Destination "$env:appdata\\adynetro\\" 
-schtasks /create /xml "$PSScriptRoot\Battery Enhancer.xml" /tn "Battery Enhancer" /ru $env:USERNAME
+Clear-Host
+for ($i = 50; $i -le 100; $i++ )
 
-Clear-Host[]
+{
+    Write-Progress -Activity "Install in progress..." -Status "$i% Complete:" -PercentComplete $i
+    Start-Sleep -Milliseconds 10
+}
+schtasks /create /xml "$PSScriptRoot\Battery Enhancer.xml" /tn "Battery Enhancer" /ru $env:USERNAME
+write-progress -Activity "Install in progress..." -Status "100% Complete:" -Completed
+Clear-Host 
 Write-Host "The script has been copied in place and scheduled to run ! Place any key to finish"
 
 [Console]::Readkey()
